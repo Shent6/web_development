7826429ccee101a47c0fcf6f5c8016ff3fb16b3f
Evgeniy Shnitko <shent6@gmail.com>
2015-10-04 21:47:40 +0300

    done cypher function

--- ceaser.py
+++ ceaser.py
@@ -1,15 +1,14 @@
 def caesar(text):
     a = ["a", "b" ,"c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    print a[25]
-#    cypher = []
-#    for i in text:
-#        ind = a.index(i)
-#        bukva = a[ind+13]
-#        cypher.append(bukva)
-#    print cypher
+    cypher = []
+    for i in text:
+        ind = a.index(i)
+        try:
+            bukva = a[ind+13]
+        except:
+            bukva = a[ind-13]
 
+        cypher.append(bukva)
+    return ''.join(cypher)
 
-
-
-
-caesar("hello")
+print caesar("hello")
