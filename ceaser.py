c6437cbfa28050005809a14eee58841d7895e4c3
Evgeniy Shnitko <shent6@gmail.com>
2015-10-04 21:31:13 +0300

    homework

---
+++ ceaser.py
@@ -0,0 +1,15 @@
+def caesar(text):
+    a = ["a", "b" ,"c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    print a[25]
+#    cypher = []
+#    for i in text:
+#        ind = a.index(i)
+#        bukva = a[ind+13]
+#        cypher.append(bukva)
+#    print cypher
+
+
+
+
+
+caesar("hello")
