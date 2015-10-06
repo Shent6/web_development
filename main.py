7c1e444116dbc26984a4fccf1137c776203ae6ca
Evgeniy Shnitko <shent6@gmail.com>
2015-10-06 22:25:54 +0300

    Fix udacity hometask(Ceaser cipher)

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ form = '''
 <form method="post">
     <label>
     ROT13
-    <input type="text" name="text" value="%(text)s">
+    <br>
+    <textarea name="text" type="text">%(text)s</textarea>
     </label>
     <br>
     <input type="submit">
@@ -16,6 +17,7 @@ form = '''
 
 def caesar(text):
     a = ["a", "b" ,"c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    cap_a = [x.upper() for x in a]
     cipher = []
     for i in text:
         if i in a:
@@ -24,6 +26,13 @@ def caesar(text):
                 bukva = a[ind+13]
             except:
                 bukva = a[ind-13]
+            cipher.append(bukva)
+        elif i in cap_a:
+            ind = cap_a.index(i)
+            try:
+                bukva = cap_a[ind+13]
+            except:
+                bukva = cap_a[ind-13]
             cipher.append(bukva)
         else:
             bukva = i
